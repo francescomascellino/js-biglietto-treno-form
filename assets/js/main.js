e68af8c525dd075175bd0829b513f1ec8138c96f
francescomascellino <franesco.mascellino@gmail.com>
2023-08-25 09:52:26 +0200

    code cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 /* 
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca nella documentazione di JS. 
+MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+La risposta finale (o output) sarà anch’essa da scrivere in solo console.
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+Questo richiederà un minimo di ricerca.
+Nota:
+Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
+Consigli:
+non usate bootstrap, fate a mano per ora meno cose dobbiamo gestire e meglio é.
+per semplificarvi un pó la vita almeno per ora non usate il tag form per racchiudere gli input ma metteteli semplicemente in un div.  div>input e non form>input
+Bonus (opzionale):
+Prova a racchiudere gli input ed il bottone in un tag form e prova a far funzionare il form evitando che la pagina si refreshi quando il form viene inviato cliccando su genera.
+Questo richiederá un minimo di ricerca per capire come usare il parametro e dentro la funzione anonima dell'event listener. function(e){ console.log(e) }
 */
 
 const ticketForm = document.getElementById("ticketForm");
@@ -102,8 +116,7 @@ ticketForm.addEventListener("submit", function(e)
             // CALCOLO DEL PREZZO  
             console.log("Il passeggero è adulto");
             const price = (distance * 0.21).toFixed(2);
-            result.innerHTML = `${passengerName}, il prezzo del biglietto per la distanza richiesta è ${price}€.`
-            ticketPrice.innerHTML = `${price} €`
+            ticketPrice.innerHTML = `${price} €`;
             console.log("Il prezzo è " + price);
 
             // IL PREZZO VIENE STAMPATO IN PAGINA E IL BIGLIETTO VIENE VISUALIZZATO
